f260be5854d6b338bff388a5d00f825964faf0e8
iabhra <rick.abhra@gmail.com>
2018-11-13 12:34:01 +0530

    Added array_gcd

--- euclidean_gcd.pl
+++ euclidean_gcd.pl
@@ -5,3 +5,15 @@ gcd(A, B, OUT):-
 	M is mod(A, B),
 	gcd(B, M, OUT).
 
+array_gcd(L, GCD):-
+	length(L, LEN),
+	LEN =:= 2,
+	[A|B] = L,
+	gcd(A, B, GCD).
+
+array_gcd(L, GCD):-
+	[L0,L1|T] = L,
+	gcd(L0, L1, G0),
+	PARTIAL = [G0|T],
+	array_gcd(PARTIAL, GCD).
+
